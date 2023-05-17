4d4a5016338643b6f588b175d7a6cc752fb29bd4
vanmeruso <vanmeruso@gmail.com>
2023-05-17 16:38:06 +0900

    feat: main & admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import LandPage from './components/LandPage';
+import Mypage from './components/MyPage'
+import Admin from './components/Admin'
+import ManageUser from './components/ManageUser'
+import ManageProblem from './components/ManageProblem'
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 const App: React.FC = () => {
-  return <LandPage />;
+  //return <Mypage />;
+  return <ManageUser/>;
+  //return <ManageProblem/>;
+  //return <Admin />;
 };
 
 export default App;
