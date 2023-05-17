4a2002a2e5c129819ace62d0d086d98add7e97e6
Jaehyun Song <tommyjr1@naver.com>
2023-05-17 05:01:39 -1000

    Change to npm

--- backend/src/problems/problems.module.ts
+++ backend/src/problems/problems.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { ProblemsService } from './problems.service';
 import { ProblemsController } from './problems.controller';
-import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   controllers: [ProblemsController],
