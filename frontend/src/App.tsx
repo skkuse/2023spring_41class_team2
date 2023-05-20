47e25d4beb1f6b4d4ba3a5bb239940fa65225925
unknown <whaihong@g.skku.edu>
2023-05-20 19:22:18 +0900

    feat: Main Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,19 @@ import ManageUser from './components/ManageUser'
 import ManageProblem from './components/ManageProblem'
 import SignupPage from './components/SignupPage'
 import ProblemPage from './components/ProblemPage'
+import MainPage from './components/MainPage'
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 const App: React.FC = () => {
   //return <Mypage />;
-  // return <ManageUser/>;
+  //return <ManageUser/>;
   //return <ManageProblem/>;
   //return <Admin />;
-  // return <LoginPage/>;
-  // return <SignupPage/>;
-  return <ProblemPage/>
+  //return <LoginPage/>;
+  //return <SignupPage/>;
+  //return <ProblemPage/>;
+  return <MainPage/>;
 };
 
 export default App;
