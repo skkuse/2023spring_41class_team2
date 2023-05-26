127355d045428c6853d914ef4c15d095a2740cff
hyeeiin <hyeinsally@g.skku.edu>
2023-05-26 13:21:48 +0900

    chore: Problem Page example code changed

--- frontend/src/components/ProblemPage.tsx
+++ frontend/src/components/ProblemPage.tsx
@@ -18,7 +18,20 @@ const ProblemPage: React.FC = () => {
               <Row>
                 <Col xs={8} className="mt-4">
                   <CodeEditor
-                      value='function add(a, b) {\n  return a + b;\n}'
+                      value='#include <unistd.h>
+#include <stdlib.h> 
+int main()
+{
+    char *buf;
+    int len;
+    /* for some file descriptor fd*/
+    read(0, &len, sizeof(len));
+
+    if (len > 8000) {return 0; }
+    buf = malloc(len);
+    read(0, buf, len);
+    return 0;
+}'
                       language="c"
                       placeholder="Type"
                       padding={15}
