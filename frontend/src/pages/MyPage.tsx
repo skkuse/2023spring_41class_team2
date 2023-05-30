c69587cc84ae790052c3c92fd29276ca6a4aa5f1
LimSeokHyun <ish990730@naver.com>
2023-05-30 19:11:09 +0900

    MyPage Update

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,85 +1,58 @@
 import React from 'react';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Form, Button, Nav } from 'react-bootstrap';
+import { Left } from 'react-bootstrap/lib/Media';
 
 const MyPage: React.FC = () => {
     return (
-        <div style={{ backgroundColor: '#EBE2E2', minHeight: '100vh' }}>
-            <Container
-                className="d-flex justify-content-center align-items-center"
-                style={{ minHeight: '100vh' }}
+        <Container
+            fluid
+            className="d-flex  "
+            style={{
+                minHeight: '100vh',
+                backgroundColor: '#EEEEEE',
+                margin: 0,
+                padding: 0,
+            }}
+        >
+            <div
+                className="d-inline"
+                style={{
+                    backgroundColor: '#EEEEEE',
+                    minHeight: '100vh',
+                    display: 'inline-block',
+                    flexGrow: 0.5,
+                }}
             >
-                <Row>
-                    <Col>
-                        <Card className="p-4" style={{ minWidth: '200px' }}>
-                            <div className="text-center mb-4">
-                                <h1
-                                    style={{
-                                        fontWeight: 'bold',
-                                        fontFamily: 'Arial, sans-serif',
-                                        display: 'inline',
-                                    }}
-                                >
-                                    My page
-                                </h1>
-                            </div>
-                            <div className="text-center mt-5">
-                                <Button
-                                    variant="secondary"
-                                    style={{
-                                        width: '100%',
-                                        minWidth: '100px',
-                                        marginBottom: '10px',
-                                    }}
-                                >
-                                    Change Username
-                                </Button>
-                                <Button
-                                    variant="secondary"
-                                    style={{
-                                        width: '100%',
-                                        minWidth: '100px',
-                                        marginBottom: '10px',
-                                    }}
-                                >
-                                    Change password
-                                </Button>
-                                <Button
-                                    variant="secondary"
-                                    style={{
-                                        width: '100%',
-                                        minWidth: '100px',
-                                        marginBottom: '10px',
-                                    }}
-                                >
-                                    My credit
-                                </Button>
-                                <Button
-                                    variant="secondary"
-                                    style={{
-                                        width: '100%',
-                                        minWidth: '100px',
-                                        marginBottom: '40px',
-                                    }}
-                                >
-                                    My Ranking
-                                </Button>
+                <Nav className="col-md-12 d-none d-md-block bg-light sidebar  ">
+                    <Nav.Item>
+                        <Nav.Link href="#"> Change Username</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Change password</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">My Ranking</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Delete My Account</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
 
-                                <Button
-                                    variant="danger"
-                                    style={{
-                                        width: '100%',
-                                        minWidth: '400px',
-                                        marginBottom: '100px',
-                                    }}
-                                >
-                                    Delete My Account
-                                </Button>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+            <Card className="p-4" style={{ minWidth: '1000px', flexGrow: 10 }}>
+                <div className="text-center mb-4 flex-fill">
+                    <h1
+                        style={{
+                            fontWeight: 'bold',
+                            fontFamily: 'Arial, sans-serif',
+                            display: 'inline',
+                        }}
+                    >
+                        My page
+                    </h1>
+                </div>
+            </Card>
+        </Container>
     );
 };
 
