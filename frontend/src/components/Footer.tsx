d7f41dc8b6e72a61cacffb9cc5397063e7c9272f
HyeongKeun Hong <whaihong@g.skku.edu>
2023-06-02 03:35:14 +0900

    chore NavBar, feat Footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Footer: React.FC = () => {
+    return(
+        <footer className="footer" style={{background: 'black', marginTop : '5px'}}>
+            <p className="copyright" style={{color: 'white', margin :'10px'}}>Copyright &copy; 2023spring 41class team2</p>
+            <p className="social-links"></p>
+               
+        </footer>
+
+
+
+    );
+};
+
+export default Footer;
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -26,11 +26,11 @@ const NavBar: React.FC = () => {
 
     return (
         <NavBarContainer>
-            <StyledText>Hello, {nickname} ! </StyledText>
             <NavBarMenuContainer>
-                <Link to="/mypage"><NavBarText>My Page</NavBarText></Link>
-                { isAdmin && <Link to="/admin"><NavBarText>Admin</NavBarText></Link> }
-                <NavBarText onClick={handleLogout}>Logout</NavBarText>
+                <StyledText style={{marginRight: '20px'}}> {nickname} </StyledText>
+                <StyledLink to="/mypage" style={{}}><NavBarText>My Page |</NavBarText></StyledLink>
+                { isAdmin && <Link to="/admin"><NavBarText>Admin |</NavBarText></Link> }
+                <StyledLink to =''> <NavBarText onClick={handleLogout}>Logout</NavBarText> </StyledLink>
             </NavBarMenuContainer>
         </NavBarContainer>
     );
@@ -54,9 +54,17 @@ const NavBarMenuContainer= styled.div`
   margin-right: 20px;
 `;
 
+const StyledLink = styled(Link)`
+    text-decoration: none;
+`;
+
 const NavBarText = styled.span`
     font-size: 20px;
     margin-right: 20px;
-    color: ${(props) => props.theme.colors.green};
+    color: ${(props) => props.theme.colors.black};
     text-decoration: none;
+    &:hover {
+        text-decoration: underline;
+    }
+    font-weight: bold;
 `
